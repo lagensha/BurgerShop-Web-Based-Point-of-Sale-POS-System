771df45d1d47f226821c579e829f89af32017154
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2025-12-11 22:47:26 +0530

    Sixth commit

--- app.js
+++ app.js
@@ -1,47 +1,150 @@
-let burgerArray=[];
-let friesArray=[];
-let drinksArray=[];
+const product=[
+    {
+        id: 1,
+        image:"C:image\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\bacon-burger-beef.jpg",
+        Name:"Bacon Burger",
+        price: 1750.00
 
-function placeOrders(){
-    let html=`
-    </div>
-<div class="card">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Name :</h5>
-    <p class="card-text">Price : </p>
-     <p class="card-text">Total : </p>
-    <a href="app.js" class="btn btn-primary">order</a>
-  </div>
-</div>
-    `;
-    let orderName=document.getElementById("cardArea").value;
-    // let orderPrice=document.getElementById("txtValue").value;
-    // let orderTotal=document.getElementById("txtTotal").value;
-}
-    placeOrders();
+    },
+     {
+        id:2,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\bacon-cheddar-jalapeno-burger.jpg",
+        name:"bacon-cheddar-jalapeno-burger",               
+        price: 2000.00
+    },
+    {
+          id:3,
+        image:"",
+        name:"chili Crunch Burger",              
+        price: 1100.00
+    },
+    {
+         id:3,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\classic.jpg",
+        name:"Classic Burger",              
+        price: 430.00
+},
+{
+    id:4,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\classic.jpg",
+        name:"Classic Burger",              
+        price: 430.00
+},
+{
+    id:5,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\double-stack-beef-burger.jpg",
+        name:"Double stack beef Burger",              
+        price: 2600.00
+},
+{
+    id:6,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\hawaiian-burger-pineapple-ham-cheese-topped-crispy-fried-onion.jpg",
+        name:"Hwaiian Burger",              
+        price: 1900.00
+},
 
-    let order=[
-    {title: "Card 1", text: "First card"},
-    {title: "Card 2", text: "Second card"},
-    {title: "Card 3", text: "Third card"}
+{
+    id:7,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\mushroom-swiss-burger.jpg",
+        name:"Mushroom Burger",                            
+        price: 1430.00
+},
 
-    ];
+{
+    id:8,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\mushroom-swiss-burger.jpg",
+        name:"Truffle Burger",                                           
+        price: 1000.00
+},
+ {
+    id:1,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Classic French Fries.jpg",
+        name:"Classic French Fries",                                        
+        price:550.00
+},
+  {
+    id:2,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Curly Fries.jpg",
+        name:"Curly Fries",
+        price:626.00                                        
+},
+   {
+    id:3,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Sweet Potato Fries.jpg,",
+        name:"Sweet Potato Fries",
+        price:1645.00                                      
+},
+    {
+    id:4,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Waffle Fries.jpg",
+        name:"Waffle Fries",
+        price:1000.00                                     
+},
+    {
+    id:1,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\coca.jpg",
+        name:"Coca-cola",
+        price:180.00                                     
+},
+   {
+    id:2,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantaorange.jpg",
+        name:"Fanta Orange",
+        price:150.00                                     
+},
+   {
+    id:3,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantapinaple.jpg",
+        name:"Fanta Pinaple",
+        price:200.00                                    
+},
+{
+    id:4,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\fantapinaple.jpg",
+        name:"Fanta Pinaple",
+        price:200.00                                    
+},
+  
+{
+    id:5,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\pepsi.jpg",
+        name:"Pepsi",
+        price:180.00                                    
+},
+{
+    id:6,
+        image:"C:\Users\lagen\Desktop\icet119\internet technology\final Pos2\asset\Cream-soda.jpg",
+        name:"Cream-Soda",
+        price:180.00                                    
+},
+];
 
-      function getdetails(){
-        let result="";
-
-       order.forEach(c =>{
-        result+=`
-        <div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title">${c.title}</h5>
-                <p class="card-text">${c.text}</p>
+const itemsCategories=[...new Set(product.map((item)=>
+{return item}))]
+let x=0;
+document.getElementById('root').innerHTML=itemsCategories.map((item)=>
+{
+     var{image,name,price}=item;
+     return(
+        `<div class='box'>
+        <div class='img-box'>
+            <img class='images' src=${image}></img>
             </div>
-        </div>`;
-    });  
-    document.getElementById("cardArea").innerHTML=result;
-      }
-      getdetails();
+            <div class='bottom'>
+            <h3>${Name}</h3>
+            <h3>$ ${Price}.00</h3>`+
+            "<button onclick ='AddOrder("+(x++)+")'>Add Order </button>"+
+            `</div>
+            </div>`
+            )
+
+}).join('')
+
+var cart=[];
+
+function display(a){
+    let i=0;
+    if(cart.length==0){
+        document.getElementById('product')
+    }
+}
